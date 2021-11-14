5225689173832d7debdb3ce2544ea98882cececb
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2021-11-14 21:45:35 +0300

    Main rightblock

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,32 +1,57 @@
 import React from "react";
-import { Box, Container, Grid, Hidden } from "@mui/material";
+import { Box, Container, Grid, Hidden, Button, Typography, TextField } from "@mui/material";
 import background from "../../Assets/Image/background.svg";
+import { useStyles } from "./useStyles";
+
 
 export const Main = () => {
+
+    const styles = useStyles();
+
     return (
         <Container>
             <Grid
                 container
                 justifyContent="space-between"
                 alignItems="center"
-                style={{ minHeight: "100vh", width: "100%" }}
+                style={{ height: "100vh", width: "100%" }}
             >
                 <Hidden mdDown>
-                    <Grid item
-                        style={{ width: "48%" }}>
+                    <Grid item md={6}>
                         <img src={background} alt="background" width="100%" />
                     </Grid>
                 </Hidden>
                 <Grid item
-                    style={{ width: "50%" }}
-                    mdDown={12}>
-                    <Box
-                        style={{
-                            width: "100%",
-                            height: "500px",
-                            backgroundColor: "blue"
-                        }}>
-                    </Box>
+                    className={styles.container_content}
+                    md={6} xs={12}>
+                    <Grid
+                        container
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="space-around"
+                        className={styles.content_block}>
+                        <Grid container
+                            direction="column"
+                            alignItems="center"
+                            textAlign="center">
+                            <Typography variant="h5" mb={2}>Начать викторину</Typography>
+                            <Typography variant="p">Для того, чтобы продолжить укажите номер викторины и нажмите кнопку "Начать викторину"</Typography>
+                        </Grid>
+                        <TextField
+                            required
+                            variant="standard"
+                            label="Номер викторины"
+                            fullWidth
+                            color="secondary" focused
+                            style={{ background: "#FFF" }} />
+
+                        <Button
+                            size="large"
+                            variant="contained"
+                            color="primary">
+                            Начать викторину
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
---
+++ src/Pages/Main/useStyles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles({
+    container_content: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    content_block: {
+        height: "80%",
+        padding: "2rem",
+        color: "white",
+        backgroundColor: "#3A6BEA",
+        borderRadius: "10px",
+    }
+});
