3838a777acccb62572be4c4c999da773eee8482c
Шумак Константин <konstantsin.shumak@gmail.com>
2021-11-24 15:50:16 +0300

    add login styles

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Hidden, Typography, TextField, Box, Button } from "@mui/material";
 import background from "../../Assets/Image/login-background.svg";
 import logo from "../../Assets/Image/logo.svg";
+import { styles } from "./styles";
 
 export const Login = () => {
 
@@ -9,18 +10,19 @@ export const Login = () => {
         <Grid
             container
             alignContent="center"
-            sx={{ minHeight: "100vh" }}
+            sx={{ ...styles.wrapper }}
         >
             <Grid
                 container
-                sx={{ height: "580px", boxShadow: 4 }}>
+                sx={{ ...styles.wrapper__container }}>
                 <Grid
                     container
                     direction="column"
                     justifyContent="center"
                     md={6}
-                    p="30px">
-                    <img src={logo} alt="login-logo" width="250px" /> <br />
+                    sx={{ ...styles.container__left_block }}
+                >
+                    <img src={logo} alt="login-logo" sx={{ ...styles.logo }} /> <br />
                     <Typography mb="45px">Welcome  back!<br />Please login/Signup to your account.</Typography>
                     <Box mb={6}>
                         <TextField
@@ -28,22 +30,23 @@ export const Login = () => {
                             label="Login/email"
                             variant="outlined"
                             fullWidth
-                            sx={{marginBottom: "10px"}}></TextField>
+                            sx={{ ...styles.block__inputs }} />
                         <TextField
                             label="Password"
                             variant="outlined"
                             type="password"
-                            fullWidth></TextField>
+                            fullWidth
+                            sx={{ ...styles.block__inputs }} />
                     </Box>
-                    <Button variant="contained" sx={{ width: "75px" }}>Login</Button>
+                    <Button variant="contained" sx={{ ...styles.button }}>Login</Button>
                 </Grid>
                 <Hidden mdDown>
                     <Grid
                         container
                         justifyContent="center"
                         md={6}
-                        sx={{ backgroundColor: "#F3F3F3" }}>
-                        <img src={background} alt="login-background" width="250px" />
+                        sx={{ ...styles.container__right_block }}>
+                        <img src={background} alt="login-background" sx={{ ...styles.right_block__image }} />
                     </Grid>
                 </Hidden>
             </Grid>
---
+++ src/Pages/Login/styles.js
@@ -0,0 +1,27 @@
+export const styles = {
+    wrapper: {
+        minHeight: "100vh",
+    },
+    wrapper__container: {
+        height: "580px",
+        boxShadow: 4,
+    },
+    container__left_block: {
+        p: "30px",
+    },
+    logo: {
+        width: "250px",
+    },
+    block__inputs: {
+        marginBottom: "10px",
+    },
+    button: {
+        width: "75px",
+    },
+    container__right_block: {
+        backgroundColor: "#F3F3F3",
+    },
+    right_block__image: {
+        width: "250px",
+    },
+}
